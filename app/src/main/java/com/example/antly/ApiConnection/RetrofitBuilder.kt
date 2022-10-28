36693cb134b54e74d3c939880f4232ee8682b904
szymonmarszalek <szymon.marszalek@vocale.app>
2022-10-28 11:52:54 +0200

    Login screen

--- app/src/main/java/com/example/antly/ApiConnection/RetrofitBuilder.kt
+++ app/src/main/java/com/example/antly/ApiConnection/RetrofitBuilder.kt
@@ -1,4 +1,4 @@
-package com.example.antly
+package com.example.antly.ApiConnection
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
--- app/src/main/java/com/example/antly/di/KoinModule.kt
+++ app/src/main/java/com/example/antly/di/KoinModule.kt
@@ -1,6 +1,6 @@
 package com.example.antly.di
 
-import com.example.antly.RetrofitBuilder
+import com.example.antly.ApiConnection.RetrofitBuilder
 import org.koin.dsl.module
 
 @JvmField
